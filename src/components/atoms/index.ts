f3a255e16d56a32f32490c3cb5bb6692bd573870
raficraft <raficraft@gmail.com>
2023-06-15 09:21:10 +0200

    bugfix: atoms alias import

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,8 +1,8 @@
-import AudioPlayer from "@atoms/AudioPlayer/AudioPlayer";
-import DropList from '@atoms/DropList/DropList';
-import Input from '@atoms/form/Input/Input';
-import SwitchButton from '@atoms/form/SwitchButton/SwitchButton';
-import Text from '@atoms/Text/Text';
+import AudioPlayer from "./AudioPlayer/AudioPlayer";
+import DropList from './DropList/DropList';
+import Input from './form/Input/Input';
+import SwitchButton from './form/SwitchButton/SwitchButton';
+import Text from './Text/Text';
 import Loading from "./Loading/Loading";
 
 export { AudioPlayer, DropList, Input, SwitchButton, Text, Loading };
