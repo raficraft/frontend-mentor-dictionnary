bee142ca1424eed708cb0fe4fffc2582da3135c1
raficraft <raficraft@gmail.com>
2023-06-15 10:03:40 +0200

    bugfix: vercel build import error

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -2,7 +2,7 @@ import AudioPlayer from "./AudioPlayer/AudioPlayer";
 import DropList from './DropList/DropList';
 import Text from './Text/Text';
 import Loading from "./Loading/Loading";
-import SwitchButton from './Form/SwitchButton/SwitchButton';
-import Input from './Form/Input/Input';
+import SwitchButton from './SwitchButton/SwitchButton';
+import Input from './Input/Input';
 
 export { AudioPlayer, DropList, Input, SwitchButton, Text, Loading };
